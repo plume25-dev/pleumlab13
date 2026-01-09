e6fb273c8020d33fccee5c8debc34063d66664de
warat wongwichit <pleumcm18@gmail.com>
2026-01-09 23:25:36 +0700

    lab_13_1_completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,40 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double a[],int N,double B[]){
+double sum =0;
+
+for(int i=0;i<N;i++)
+{
+    sum =sum +a[i];
+}
+long double max = a[0],min =a[0];
+for(int i=0;i<N;i++){
+    if(a[i]>max) max=a[i];
+    if(a[i]<min) min=a[i];
+}
+double mean =sum/N;
+double sum_sq = 0;
+    for(int i = 0; i < N; i++){
+        sum_sq += (a[i] - mean) * (a[i] - mean);
+    }
+double product = 1;
+    for(int i = 0; i < N; i++){
+        product *= a[i];
+    }
+double hm_sum = 0;
+    for(int i = 0; i < N; i++){
+        hm_sum += 1.0 / a[i];
+    }
+
+double hm = N / hm_sum;
+double gm = pow(product, 1.0 / N);
+double sd = sqrt(sum_sq / (N));
+B[0]=sum/N;
+B[1]=sd;
+B[2]=gm;
+B[3]=hm;
+B[4]=max;
+B[5]=min;
+}
+
